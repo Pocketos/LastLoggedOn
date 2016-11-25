e5a2f3a8b7d29ed69b804165d7975551d0116f02
Tyler <tylermalcarne@outlook.com>
2016-11-25 14:46:11 -0500

    Added Application Icons Added catch to file denied exception thrown when clicking replace if not running as administrator. Made UI elements bigger. Added a help button.

--- LastLoggedon/Form1.Designer.vb
+++ LastLoggedon/Form1.Designer.vb
@@ -22,45 +22,49 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.btn_replace = New System.Windows.Forms.Button()
         Me.txt_username = New System.Windows.Forms.TextBox()
         Me.btn_check = New System.Windows.Forms.Button()
         Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
         Me.tsslbl_status = New System.Windows.Forms.ToolStripStatusLabel()
         Me.btn_logoff = New System.Windows.Forms.Button()
+        Me.btn_help = New System.Windows.Forms.Button()
         Me.StatusStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'btn_replace
         '
+        Me.btn_replace.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btn_replace.Location = New System.Drawing.Point(12, 38)
         Me.btn_replace.Name = "btn_replace"
-        Me.btn_replace.Size = New System.Drawing.Size(55, 23)
+        Me.btn_replace.Size = New System.Drawing.Size(99, 33)
         Me.btn_replace.TabIndex = 0
-        Me.btn_replace.Text = "Reaplce"
+        Me.btn_replace.Text = "Replace"
         Me.btn_replace.UseVisualStyleBackColor = True
         '
         'txt_username
         '
-        Me.txt_username.Location = New System.Drawing.Point(12, 12)
+        Me.txt_username.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txt_username.Location = New System.Drawing.Point(12, 7)
         Me.txt_username.Name = "txt_username"
-        Me.txt_username.Size = New System.Drawing.Size(220, 20)
+        Me.txt_username.Size = New System.Drawing.Size(220, 26)
         Me.txt_username.TabIndex = 1
         Me.txt_username.Text = "Username"
         '
         'btn_check
         '
-        Me.btn_check.Location = New System.Drawing.Point(73, 38)
+        Me.btn_check.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_check.Location = New System.Drawing.Point(117, 38)
         Me.btn_check.Name = "btn_check"
-        Me.btn_check.Size = New System.Drawing.Size(93, 23)
+        Me.btn_check.Size = New System.Drawing.Size(115, 33)
         Me.btn_check.TabIndex = 2
         Me.btn_check.Text = "Check Value"
         Me.btn_check.UseVisualStyleBackColor = True
         '
         'StatusStrip1
         '
-        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsslbl_status})
-        Me.StatusStrip1.Location = New System.Drawing.Point(0, 67)
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 114)
         Me.StatusStrip1.Name = "StatusStrip1"
         Me.StatusStrip1.Size = New System.Drawing.Size(244, 22)
         Me.StatusStrip1.TabIndex = 3
@@ -69,32 +73,43 @@ Partial Class Form1
         'tsslbl_status
         '
         Me.tsslbl_status.Name = "tsslbl_status"
-        Me.tsslbl_status.Size = New System.Drawing.Size(64, 17)
-        Me.tsslbl_status.Text = "Version 1.0"
+        Me.tsslbl_status.Size = New System.Drawing.Size(63, 17)
+        Me.tsslbl_status.Text = "Version 1.2"
         '
         'btn_logoff
         '
-        Me.btn_logoff.Location = New System.Drawing.Point(172, 38)
+        Me.btn_logoff.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_logoff.Location = New System.Drawing.Point(12, 77)
         Me.btn_logoff.Name = "btn_logoff"
-        Me.btn_logoff.Size = New System.Drawing.Size(60, 23)
+        Me.btn_logoff.Size = New System.Drawing.Size(220, 28)
         Me.btn_logoff.TabIndex = 4
         Me.btn_logoff.Text = "Logoff"
         Me.btn_logoff.UseVisualStyleBackColor = True
+        '
+        'btn_help
+        '
+        Me.btn_help.Location = New System.Drawing.Point(169, 114)
+        Me.btn_help.Name = "btn_help"
+        Me.btn_help.Size = New System.Drawing.Size(75, 23)
+        Me.btn_help.TabIndex = 5
+        Me.btn_help.Text = "Help"
+        Me.btn_help.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(244, 89)
+        Me.ClientSize = New System.Drawing.Size(244, 136)
+        Me.Controls.Add(Me.btn_help)
         Me.Controls.Add(Me.btn_logoff)
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.btn_check)
         Me.Controls.Add(Me.txt_username)
         Me.Controls.Add(Me.btn_replace)
-        Me.MaximumSize = New System.Drawing.Size(260, 128)
-        Me.MinimumSize = New System.Drawing.Size(260, 128)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(260, 175)
+        Me.MinimumSize = New System.Drawing.Size(260, 175)
         Me.Name = "Form1"
-        Me.Text = "LastLoggedOn"
         Me.StatusStrip1.ResumeLayout(False)
         Me.StatusStrip1.PerformLayout()
         Me.ResumeLayout(False)
@@ -108,4 +123,5 @@ Partial Class Form1
     Friend WithEvents StatusStrip1 As StatusStrip
     Friend WithEvents tsslbl_status As ToolStripStatusLabel
     Friend WithEvents btn_logoff As Button
+    Friend WithEvents btn_help As Button
 End Class
--- LastLoggedon/Form1.vb
+++ LastLoggedon/Form1.vb
@@ -4,6 +4,7 @@ End Module
 
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
         userkey = My.Computer.Registry.GetValue(
                 "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI", "LastLoggedOnUser", Nothing)
         txt_username.Text = userkey
@@ -12,18 +13,26 @@ Public Class Form1
 
     Private Sub btn_replace_Click(sender As Object, e As EventArgs) Handles btn_replace.Click
         userkey = txt_username.Text
-        My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI", "LastLoggedOnUser", userkey)
+        Try
+            My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI", "LastLoggedOnUser", userkey)
+        Catch Exc As System.UnauthorizedAccessException
+            MsgBox("An permission error has occured.  Is this application being run as Administrator?", , "!")
+        End Try
     End Sub
 
     Private Sub btn_check_Click(sender As Object, e As EventArgs) Handles btn_check.Click
         If My.Computer.Registry.GetValue(
                 "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI", "LastLoggedOnUser", Nothing) Is Nothing Then
-            MsgBox("Value Doen't Exist")
+            MsgBox("Value Doen't Exist", , My.Application.Info.AssemblyName.ToString)
         End If
         MsgBox(userkey)
     End Sub
 
     Private Sub btn_logoff_Click(sender As Object, e As EventArgs) Handles btn_logoff.Click
         Shell("shutdown -l")
+    End Sub
+
+    Private Sub btn_help_Click(sender As Object, e As EventArgs) Handles btn_help.Click
+        MsgBox("Click Check Vaue to see the currently last logged on user." & Environment.NewLine & "Click Replace to change the currently Last Logged on User to whatever is in the text box" & Environment.NewLine & "Click Loggout once the value is replaced to logout.", , My.Application.Info.AssemblyName.ToString & " --Help")
     End Sub
 End Class
